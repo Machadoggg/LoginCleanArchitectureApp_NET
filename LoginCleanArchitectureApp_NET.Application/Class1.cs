19b260abb83881a53322d888d19d50ad4a6ca267
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 20:43:23 -0500

    Add AuthService class

--- LoginCleanArchitectureApp_NET.Application/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LoginCleanArchitectureApp_NET.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ LoginCleanArchitectureApp_NET.Application/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using LoginCleanArchitectureApp_NET.Core.Entities;
+using LoginCleanArchitectureApp_NET.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace LoginCleanArchitectureApp_NET.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AuthService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        public async Task<User> Authenticate(string username, string password)
+        {
+            return await _userRepository.Authenticate(username, password);
+        }
+    }
+}
