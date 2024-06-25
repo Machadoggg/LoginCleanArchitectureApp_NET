c8d829ddbae5837048e4848615faab341b69824b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 19:24:10 -0500

    Add IUserRepository interface

--- LoginCleanArchitectureApp_NET.Core/Interfaces/IUserRepository.cs
+++ LoginCleanArchitectureApp_NET.Core/Interfaces/IUserRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace LoginCleanArchitectureApp_NET.Core.Interfaces
+﻿using LoginCleanArchitectureApp_NET.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LoginCleanArchitectureApp_NET.Core.Interfaces
 {
     public interface IUserRepository
     {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
     }
 }
