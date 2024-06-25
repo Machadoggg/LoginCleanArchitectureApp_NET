2cbc44e20681e933c3e52cae1c1eff1d4b738710
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 19:35:43 -0500

    Add DbContext configuration

---
+++ LoginCleanArchitectureApp_NET.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginCleanArchitectureApp_NET.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginCleanArchitectureApp_NET.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
